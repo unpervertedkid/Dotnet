82da34a2400a9280355f921e686d458ec2e2c28b
Brian Silah <unpervertedkid@gmail.com>
2022-11-16 14:44:27 +0300

    Debugging

--- DotNetDebugging/Program.cs
+++ DotNetDebugging/Program.cs
@@ -1,7 +1,11 @@
-﻿int result = Fibonacci(5);
+﻿using System.Diagnostics;
+
+int result = Fibonacci(5);
 Console.WriteLine(result);
 
 static int Fibonacci(int number){
+    Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+    Debug.WriteLine($"We are looking for the {number}th number");
     int n1 = 0;
     int n2 = 1;
     int sum;
@@ -10,6 +14,9 @@ static int Fibonacci(int number){
         sum = n1 + n2;
         n1 = n2;
         n2 = sum;
+            Debug.WriteLineIf(sum == 1, $"Sum is 1, n1 is {n1}, n2 is {n2}");
+
     }
+    Debug.Assert(n2 == 5, "The return value is supposed to be 5 and its not");
     return number == 0 ? n1 : n2;
 }
